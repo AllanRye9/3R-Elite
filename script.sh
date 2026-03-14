f18b04e6e9de9bd75539d11bb4a8b759f53afad2
Oryema Allan <34004636+AllanRye9@users.noreply.github.com>
2026-03-14 03:47:59 +0000

    rebuild deps 2026-03-14 03:47

---
+++ script.sh
@@ -0,0 +1,4 @@
+git pull && \
+cd frontend && rm -rf node_modules package-lock.json && npm install && \
+cd ../backend && rm -rf node_modules && npm install && \
+cd .. && git add . && git commit -m "rebuild deps $(date '+%Y-%m-%d %H:%M')" && git push
